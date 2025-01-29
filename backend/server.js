eddbd08cf8378a744652d7f87beb3b99582f7e47
Shozab <shozabali06@gmail.com>
2025-01-29 19:42:32 +0500

    add backend setup; implement database and Cloudinary connection; configure server with Express and middleware

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./configs/db.js";
+import connectCloudinary from "./configs/cloudinary.js";
+
+// App Config
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Connect DB
+connectDB();
+
+// Connect Cloudinary
+connectCloudinary
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// Api Endpoints
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+
+app.listen(port, () => {
+  console.log(`Server started at port ${port}`);
+});
