21b1c4ce5494689c3317ce7f5b1638533316a34c
Shozab <shozabali06@gmail.com>
2025-01-30 19:58:35 +0500

    add user authentication; implement user registration and login routes; create user and product models

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./configs/db.js";
 import connectCloudinary from "./configs/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 // App Config
 const app = express();
@@ -12,13 +13,15 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 // Connect Cloudinary
-connectCloudinary
+connectCloudinary;
 
 // Middlewares
 app.use(express.json());
 app.use(cors());
 
 // Api Endpoints
+app.use("/api/user", userRouter);
+
 app.get("/", (req, res) => {
   res.send("API WORKING");
 });
