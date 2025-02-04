23ecb15764d54a98ff1c7d65aadbf17a99dc4440
Shozab <shozabali06@gmail.com>
2025-02-04 19:42:57 +0500

    add admin authentication; implement admin login functionality and middleware for protected routes

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,11 +6,13 @@ import {
   addProduct,
 } from "../controllers/productController.js";
 import upload from "../middlewares/multer.js";
+import adminAuth from "../middlewares/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post(
   "/add",
+  adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -19,8 +21,8 @@ productRouter.post(
   ]),
   addProduct
 );
-productRouter.post("/remove", removeProduct);
-productRouter.post("/single", singleProduct);
-productRouter.get("/list", listProducts);
+productRouter.post("/remove", adminAuth, removeProduct);
+productRouter.post("/single", adminAuth, singleProduct);
+productRouter.get("/list", adminAuth, listProducts);
 
 export default productRouter;
